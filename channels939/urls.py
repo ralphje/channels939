66fdeacb44cb7c547bd10a09a31c9f957d095f69
Ralph Broenink <ralph@ralphbroenink.net>
2018-03-04 13:27:39 +0100

    More minimalistic settings and less files

--- channels939/urls.py
+++ channels939/urls.py
@@ -13,12 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
 
 from example.views import StuffView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', StuffView.as_view())
 ]
